09a145b11615494df83f7f6a24fbf50f8ffc8f9f
Oksana Slusarenro <oksana.slu@gmail.com>
2017-11-20 15:24:54 +0200

    Add tests for media_service.

---
+++ azure_video_pipeline/tests/unit/tests_media_service.py
@@ -0,0 +1,205 @@
+import unittest
+
+from azure_video_pipeline.media_service import AccessPolicyPermissions, LocatorTypes, MediaServiceClient
+from freezegun import freeze_time
+import mock
+from requests import HTTPError
+
+
+class MediaServiceClientTests(unittest.TestCase):
+
+    @mock.patch('azure_video_pipeline.media_service.ServicePrincipalCredentials')
+    def make_one(self, service_principal_credentials):
+        azure_config = {
+            'client_id': 'client_id',
+            'secret': 'client_secret',
+            'tenant': 'tenant',
+            'rest_api_endpoint': 'https://rest_api_endpoint/api/',
+            'storage_account_name': 'storage_account_name',
+            'storage_key': 'storage_key'
+        }
+        media_services = MediaServiceClient(azure_config)
+        media_services.credentials = mock.Mock(token={'token_type': 'token_type', 'access_token': 'access_token'})
+        return media_services
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.get',
+                return_value=mock.Mock(status_code=400, raise_for_status=mock.Mock(side_effect=HTTPError)))
+    @mock.patch('azure_video_pipeline.media_service.requests.post',
+                return_value=mock.Mock(status_code=400, raise_for_status=mock.Mock(side_effect=HTTPError)))
+    def raise_for_status(self, requests_post, requests_get, headers, func, func_args=None):
+        media_services = self.make_one()
+        with self.assertRaises(HTTPError):
+            if func_args:
+                getattr(media_services, func)(*func_args)
+            else:
+                getattr(media_services, func)()
+
+    def test_get_headers(self):
+        media_services = self.make_one()
+        headers = media_services.get_headers()
+        expected_headers = {
+            'Content-Type': 'application/json',
+            'DataServiceVersion': '1.0',
+            'MaxDataServiceVersion': '3.0',
+            'Accept': 'application/json',
+            'Accept-Charset': 'UTF-8',
+            'x-ms-version': '2.15',
+            'Host': 'rest_api_endpoint',
+            'Authorization': 'token_type access_token'
+        }
+        self.assertEqual(headers, expected_headers)
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.get',
+                return_value=mock.Mock(status_code=200,
+                                       json=mock.Mock(return_value={'value': ['locator1', 'locator2']})))
+    def test_get_locators_list(self, requests_get, headers):
+        media_services = self.make_one()
+        locators = media_services.get_locators_list(LocatorTypes.OnDemandOrigin)
+        requests_get.assert_called_once_with('https://rest_api_endpoint/api/Locators?$filter=Type eq 2', headers={})
+        self.assertEqual(locators, ['locator1', 'locator2'])
+
+    def test_raise_for_status_get_list_locators(self):
+        self.raise_for_status(func='get_locators_list')
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.get',
+                return_value=mock.Mock(status_code=200,
+                                       json=mock.Mock(return_value={'value': ['locator']})))
+    def test_get_asset_locator(self, requests_get, headers):
+        media_services = self.make_one()
+        asset_id = 'asset_id'
+        locator = media_services.get_asset_locator(asset_id, LocatorTypes.SAS)
+        requests_get.assert_called_once_with(
+            "https://rest_api_endpoint/api/Assets('{}')/Locators?$filter=Type eq 1".format(asset_id),
+            headers={}
+        )
+        self.assertEqual(locator, 'locator')
+
+    def test_raise_for_status_get_asset_locator(self):
+        self.raise_for_status(func='get_asset_locator', func_args=['asset_id', '1'])
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.get',
+                return_value=mock.Mock(status_code=200,
+                                       json=mock.Mock(return_value={'value': ['file1', 'file2']})))
+    def test_get_asset_files(self, requests_get, headers):
+        media_services = self.make_one()
+        asset_id = 'asset_id'
+        files = media_services.get_asset_files(asset_id)
+        requests_get.assert_called_once_with(
+            "https://rest_api_endpoint/api/Assets('{}')/Files".format(asset_id),
+            headers={}
+        )
+        self.assertEqual(files, ['file1', 'file2'])
+
+    def test_raise_for_status_get_asset_files(self):
+        self.raise_for_status(func='get_asset_files', func_args=['asset_id'])
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.post',
+                return_value=mock.Mock(status_code=201,
+                                       json=mock.Mock(return_value={'asset_id': 'asset_id',
+                                                                    'asset_name': 'asset_name'})))
+    def test_create_asset(self, requests_post, headers):
+        media_services = self.make_one()
+        asset_name = 'asset_name'
+        asset = media_services.create_asset(asset_name)
+        requests_post.assert_called_once_with(
+            "https://rest_api_endpoint/api/Assets",
+            headers={},
+            json={'Name': asset_name}
+        )
+        self.assertEqual(asset, {'asset_id': 'asset_id', 'asset_name': 'asset_name'})
+
+    def test_raise_for_status_create_assets(self):
+        self.raise_for_status(func='create_asset', func_args=['asset_name'])
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.post',
+                return_value=mock.Mock(status_code=201,
+                                       json=mock.Mock(return_value={'file_id': 'file_id',
+                                                                    'file_name': 'file_name'})))
+    def test_create_asset_file(self, requests_post, headers):
+        media_services = self.make_one()
+        asset_id = 'asset_id'
+        file_name = 'file_name'
+        mime_type = 'mime_type'
+        asset = media_services.create_asset_file(asset_id, file_name, mime_type)
+        expected_data = {
+            "IsEncrypted": "false",
+            "IsPrimary": "false",
+            "MimeType": mime_type,
+            "Name": file_name,
+            "ParentAssetId": asset_id
+        }
+        requests_post.assert_called_once_with(
+            "https://rest_api_endpoint/api/Files",
+            headers={},
+            json=expected_data
+        )
+        self.assertEqual(asset, {'file_id': 'file_id', 'file_name': 'file_name'})
+
+    def test_raise_for_status_create_asset_file(self):
+        self.raise_for_status(func='create_asset_file', func_args=['asset_id', 'file_name', 'mime_type'])
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.post',
+                return_value=mock.Mock(status_code=201,
+                                       json=mock.Mock(return_value={'policy_id': 'policy_id',
+                                                                    'policy_name': 'policy_name'})))
+    def test_create_access_policy(self, requests_post, headers):
+        media_services = self.make_one()
+        policy_name = 'policy_name'
+        asset = media_services.create_access_policy(policy_name)
+        expected_data = {
+            "Name": policy_name,
+            "DurationInMinutes": 120,
+            "Permissions": AccessPolicyPermissions.NONE
+        }
+        requests_post.assert_called_once_with(
+            "https://rest_api_endpoint/api/AccessPolicies",
+            headers={},
+            json=expected_data
+        )
+        self.assertEqual(asset, {'policy_id': 'policy_id', 'policy_name': 'policy_name'})
+
+    def test_raise_for_status_create_access_policy(self):
+        self.raise_for_status(func='create_access_policy', func_args=['policy_name'])
+
+    @mock.patch('azure_video_pipeline.media_service.MediaServiceClient.get_headers',
+                return_value={})
+    @mock.patch('azure_video_pipeline.media_service.requests.post',
+                return_value=mock.Mock(status_code=201,
+                                       json=mock.Mock(return_value={'locator_id': 'locator_id',
+                                                                    'locator_name': 'locator_name'})))
+    @freeze_time("2017-11-01")
+    def test_create_locator(self, requests_post, headers):
+        media_services = self.make_one()
+        access_policy_id = 'access_policy_id'
+        asset_id = 'asset_id'
+        locator_type = 'locator_type'
+        asset = media_services.create_locator(access_policy_id, asset_id, locator_type)
+        expected_data = {
+            "AccessPolicyId": access_policy_id,
+            "AssetId": asset_id,
+            "StartTime": '2017-10-31T23:50:00',
+            "Type": locator_type
+        }
+        requests_post.assert_called_once_with(
+            "https://rest_api_endpoint/api/Locators",
+            headers={},
+            json=expected_data
+        )
+        self.assertEqual(asset, {'locator_id': 'locator_id', 'locator_name': 'locator_name'})
+
+    def test_raise_for_status_create_locator(self):
+        self.raise_for_status(func='create_locator', func_args=['access_policy_id', 'asset_id', 'locator_type'])
