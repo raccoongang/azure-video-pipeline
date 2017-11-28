628d8c17f5f4b81ad0e07d70a5807c7f75bc1f59
wowkalucky <wowkalucky@gmail.com>
2017-11-28 15:59:19 +0200

    Fix test environment settings check

--- azure_video_pipeline/__init__.py
+++ azure_video_pipeline/__init__.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from mock import Mock
 
-modules_to_mock = getattr(settings, 'MOCKED_MODULES')
+modules_to_mock = getattr(settings, 'MOCKED_MODULES', None)
 if modules_to_mock:
     import sys
     for module_name in modules_to_mock:
